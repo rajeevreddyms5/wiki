62a3ab349e90a26ea63069516ea0eaebc11bf128
MMEGS <rajeevreddyms5@gmail.com>
2023-07-11 16:48:51 +0530

    Entry Page completed

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -9,4 +9,9 @@ def index(request):
     })
 
 def view(request, name):
-    return HttpResponse(util.get_entry(name))
+    if util.get_entry(name):
+        line1 = util.get_entry(name).split("\n")[0]
+        title = line1.split(" ")[1]
+        return HttpResponse(f"<title>{title}</title> {util.get_entry(name)}")
+    else:
+        return HttpResponse("<h1>Requested page was not found</h1>")
